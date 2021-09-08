1cd105e1545cec29ef619ed9caaefe1700f8ef50
MaximPerry <maxim_perry@live.ca>
2021-09-08 15:16:04 -0400

    Converted extension for cross-browsers

--- scripts/content_script.js
+++ scripts/content_script.js
@@ -2,4 +2,4 @@ filter = false;
 function BWFilter() {
     if (filter == false) { document.body.style.filter = "grayscale(100%)"; filter = true; }
     else { document.body.style.filter = "none"; filter = false; }
-} browser.runtime.onConnect.addListener(BWFilter);
+} chrome.runtime.onConnect.addListener(BWFilter);
